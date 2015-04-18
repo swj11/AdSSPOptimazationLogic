cec5f7a31edc0b797415973f84c5185b9d0b8ea9
songwonje <songwonje@songwonjeui-iMac.local>
2015-04-18 11:35:23 +0900

    Initial Commit

---
+++ AdSSPOptimazationLogic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdSSPOptimazationLogic
+//
+//  Created by songwonje on 2015. 4. 18..
+//  Copyright (c) 2015년 songwonje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
